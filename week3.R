1aba384ed25acdd1655173d83073e5c70cfee553
iomkarsurve@gmail.com <iomkarsurve@gmail.com>
2020-04-18 23:16:11 +0530

    along with week 4

--- week3.R
+++ week3.R
@@ -82,6 +82,56 @@ axis(side=1,at=seq(0,11,1))
 axis(side=2,at=seq(0,16,2))
 
 #ggplot2
+library(ggplot2)
+
+ggplot() + geom_histogram(data = g, aes(x = fruitveg),bins = 10, fill = "darkgreen",col = "black")+
+labs(x = "Portions of fruit and vegetables", y = "Frequency")+
+  scale_x_continuous(breaks = seq(from = 0, to = 12, by = 0.5))+ theme_bw()
 
 
+hist(g$fruit, xlab = "Portions of fruit",
+     
+     main = "Daily consumption of fruit", axes = F)
+
+axis(side = 1, at = seq(0, 4, 1))
+
+axis(side = 2, at = seq(0, 24, 4))
+
+
+hist(g$veg, xlab = "Portions of veg",
+     
+     main = "Daily consumption of veg", axes = F)
+
+axis(side = 1, at = seq(0, 9, 1))
+
+axis(side = 2, at = seq(0, 18, 2))
+
+#healthy bmi
+
+healthy_bmi<- ifelse(g$bmi > 18.5 & g$bmi < 25,1,0)
+table(healthy_bmi)
+
+hist(healthy_bmi)
+
+
+chisq.test(x=five_a_day,y=g$cancer) 
+
+t.test(g$bmi~g$cancer,var.equal=T)
+
+t.test(g$bmi~g$cancer)
+
+overw<- ifelse(g$bmi>25,1,0)
+
+t.test(overw~g$cancer)
+
+t.test(g$bmi,mu=25)
+
+
+cancer <- g$cancer
+
+overweight <- ifelse(g$bmi >= 25, 1, 0)
+
+table(overweight)
+
+chisq.test(x=overweight,y=cancer)
 
